ff592157e9b255113120c7e06c6e958a062e7b5a
Ilya <ilyagonnov@gmail.com>
2021-12-09 21:19:18 +0400

    подключил react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/enquiries">Enquiry</Link>
+        <Switch>
+          <Route path="/enquiries">enquiries</Route>
+          <Route path="/">home</Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
