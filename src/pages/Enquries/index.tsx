ef13974e4f4189cfb2386254eed5f41a5f0d60ac
Ilya <ilyagonnov@gmail.com>
2021-12-10 00:35:42 +0400

    fetch data on enquries page

--- src/pages/Enquries/index.tsx
+++ src/pages/Enquries/index.tsx
@@ -1,11 +1,46 @@
 import React, { FC } from "react";
 
-type EnquriesProps = {
-  className?: string;
-};
+interface Enqury {
+  name: string;
+  age: number;
+  done: boolean;
+}
 
-const Enquries: FC<EnquriesProps> = (props): JSX.Element => {
-  return <div className="Enquries">Enquries</div>;
+const Enquries: FC = (props): JSX.Element => {
+  const [enquryList, setEnquryList] = React.useState<Enqury[]>([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState("");
+
+  React.useEffect(() => {
+    getEnquryList();
+  }, []);
+
+  async function getEnquryList() {
+    setIsLoading(true);
+    try {
+      const res = await fetch("/enquries", {
+        method: "Post",
+      });
+      const data = await res.json();
+      setEnquryList(data);
+    } catch (e) {
+      if (e instanceof Error) {
+        setErrorMessage(e.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div className="Enquries">
+      {errorMessage}
+      {isLoading && "loading..."}
+      {enquryList.map((enqury) => {
+        return <div>{enqury.name}</div>;
+      })}
+    </div>
+  );
 };
 
 export default Enquries;
